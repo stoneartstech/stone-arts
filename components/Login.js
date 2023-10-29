955c55cf23cc521345cdb286d3e762707f518984
swapnilgarg7 <swapnilgarg810@gmail.com>
2023-10-29 18:39:46 +0530

    mobile fix

--- components/Login.js
+++ components/Login.js
@@ -25,11 +25,11 @@ function Login() {
         <div className='flex flex-col justify-center items-center'>
             <p className='font-bold text-3xl mb-8'>LOGIN</p>
             <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}
-                placeholder="email" className=' p-2 w-full max-w-[25vw]' />
+                placeholder="email" className=' p-2 w-full sm:max-w-[25vw]' />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}
-                placeholder="password" className=' p-2 w-full max-w-[25vw] my-4' />
-            {error && <p className='text-red-500 w-full max-w-[25vw]'>{error}</p>}
-            <button onClick={submitHandler} className='bg-slate-300 hover:bg-slate-400 p-2 w-full max-w-[25vw]'>Login</button>
+                placeholder="password" className=' p-2 w-full sm:max-w-[25vw] my-4' />
+            {error && <p className='text-red-500 w-full sm:max-w-[25vw]'>{error}</p>}
+            <button onClick={submitHandler} className='bg-slate-300 hover:bg-slate-400 p-2 w-full sm:max-w-[25vw]'>Login</button>
             {/* <p className='my-8 text-xl'>OR</p>
             <a href="/clientform" className='bg-slate-900 hover:bg-slate-700 text-white p-3 w-full max-w-[25vw] text-center'>
                 Client Form
