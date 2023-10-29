96ac6201e488e091f7493183a7694bda7ea0f617
swapnilgarg7 <swapnilgarg810@gmail.com>
2023-10-29 19:18:42 +0530

    fixed options error

--- components/AdminDashboard.js
+++ components/AdminDashboard.js
@@ -1,23 +1,25 @@
 import React from 'react'
 
 function AdminDashboard() {
-    const options = {
-        "Sales": '/saleshome',
-        "Design": '/designhome',
-        "BOQ": '/boqhome',
-        "PMT": '/pmthome',
-        "Workshop": '/workshophome',
-        "Logistics": '/logisticshome',
-        "Drivers": '/drivershome',
-    }
+    const options = [
+        { title: "Sales", link: '/saleshome' },
+        { title: "Design", link: '/designhome' },
+        { title: "BOQ", link: '/boqhome' },
+        { title: "PMT", link: '/pmthome' },
+        { title: "Workshop", link: '/workshophome' },
+        { title: "Logistics", link: '/logisticshome' },
+        { title: "Drivers", link: '/drivershome' }
+    ]
+
+
     return (
         <div>
             <p className='text-2xl text-center mb-8'>Admin Home Page</p>
             <div className='flex flex-col text-xl gap-4 items-center'>
-                {Object.keys(options).map((option) => (
-                    <a href={options[option]}
+                {options.map((option) => (
+                    <a href={option.link}
                         className='bg-slate-300 hover:bg-slate-400 p-2 w-full max-w-[25vw]'>
-                        {option}
+                        {option.title}
                     </a>
                 ))}
             </div>
