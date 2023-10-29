7999e64d3a7d904e2e325131b9112d58f611d1b5
swapnilgarg7 <swapnilgarg810@gmail.com>
2023-10-29 19:15:02 +0530

    changed link

--- pages/success.js
+++ pages/success.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function success() {
     return (
         <div className='text-3xl text-center flex flex-col items-center justify-center'>
             Information sent to BOQ Successfully !
-            <a href="/" className='bg-slate-300 hover:bg-slate-400 p-2 w-1/3 mt-12'>
+            <Link href="/" className='bg-slate-300 hover:bg-slate-400 p-2 w-1/3 mt-12'>
                 Back to Home
-            </a>
+            </Link>
         </div>)
 
 }
