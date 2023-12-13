b294a8d08d35727e3654138a3cdbd4d002fab4d0
swapnilgarg7 <swapnilgarg810@gmail.com>
2023-12-13 18:10:43 +0530

    converted data to table

--- components/ClientHistory.js
+++ components/ClientHistory.js
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { db } from '../firebase'
-import { collection, getDocs, onSnapshot } from 'firebase/firestore'
+import React, { useState, useEffect, useMemo } from 'react';
+import { useTable } from 'react-table';
+import { db } from '../firebase';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 export default function ClientHistory({ showroomName }) {
-
-    const [clientRequests, setClientRequests] = useState([])
-    const [originalClientRequests, setOriginalClientRequests] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [search, setSearch] = useState('')
+    const [clientRequests, setClientRequests] = useState([]);
+    const [originalClientRequests, setOriginalClientRequests] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         const fetch = onSnapshot(collection(db, 'clients'), (snapshot) => {
@@ -17,6 +16,11 @@ export default function ClientHistory({ showroomName }) {
                 ...doc.data()
             }))
             clientRequests = clientRequests.filter((clientRequest) => clientRequest.showroom === showroomName)
+            clientRequests.forEach((clientRequest) => {
+                clientRequest.aspects = clientRequest.aspects.join(',')
+                clientRequest.date = clientRequest.date
+            })
+            console.log(clientRequests)
 
             setClientRequests(clientRequests)
             setOriginalClientRequests(clientRequests)
@@ -26,23 +30,37 @@ export default function ClientHistory({ showroomName }) {
         return fetch
     }, [])
 
-    // async function getClientRequests() {
-    //     const querySnapshot = await getDocs(collection(db, 'clients'));
-    //     querySnapshot.forEach((doc) => {
-    //         console.log(doc.id, ' => ', doc.data());
-    //     });
-    // };
-    function handleButtonClick(name) {
-        clientRequests.forEach((request) => {
-            if (request.name === name) {
-                request.showDetails = !request.showDetails;
-            }
-        });
-        setClientRequests([...clientRequests]);
-    }
+    const data = useMemo(() => clientRequests, [clientRequests]);
+
+    const columns = useMemo(
+        () => [
+            { Header: 'Client ID', accessor: 'clientId', minSize: 50 },
+            { Header: 'Client Name', accessor: 'name', minSize: 200 },
+            { Header: 'Client Email', accessor: 'email', minSize: 200 },
+            { Header: 'Client Number', accessor: 'number', minSize: 200 },
+            { Header: 'Date of Request', accessor: 'date', minSize: 200 },
+            { Header: 'Interested Aspects', accessor: "aspects", minSize: 200 },
+            { Header: 'Request Category', accessor: 'option', minSize: 200 },
+            { Header: 'Client Source', accessor: 'sourceInfo', minSize: 200 },
+            { Header: '(Other Source)', accessor: 'specificInfo', minSize: 200 },
+            { Header: 'Measurement Cost', accessor: 'measurementData.cost', minSize: 200 },
+            { Header: 'Measurement Date', accessor: 'measurementData.date', minSize: 200 },
+            { Header: 'Measurement Time', accessor: 'measurementData.time', minSize: 200 },
+            { Header: 'Measurement Supply/Fix', accessor: 'measurementData.supplyFix', minSize: 200 },
+            { Header: 'Measurement Contact Person', accessor: 'measurementData.contactPerson', minSize: 200 },
+        ],
+        []
+    );
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({ columns, data });
 
     function handleSearch() {
-
         setClientRequests(originalClientRequests.filter((clientRequest) => {
             var searchParam = search.toLowerCase()
             return (clientRequest.name.toLowerCase().includes(searchParam)
@@ -51,7 +69,7 @@ export default function ClientHistory({ showroomName }) {
                 || clientRequest.number.toString().includes(searchParam)
                 || clientRequest.address.toLowerCase().includes(searchParam)
                 || clientRequest.date.toString().toLowerCase().includes(searchParam)
-                || clientRequest.aspects.join('').toLowerCase().includes(searchParam)
+                || clientRequest.aspects.toLowerCase().includes(searchParam)
                 || clientRequest.option.toLowerCase().includes(searchParam)
                 || clientRequest.sourceInfo.toLowerCase().includes(searchParam)
             )
@@ -59,57 +77,63 @@ export default function ClientHistory({ showroomName }) {
         ))
     }
 
+    return (
+        <>
+            {!loading && (
+                <div>
+                    <p className='mt-8 text-2xl text-center font-bold mb-4'>
+                        Requests from Clients
+                    </p>
+                    <div className='flex flex-col gap-4 '>
+                        <div className='mx-auto'>
+                            <input
+                                onChange={(e) => setSearch(e.target.value)}
+                                className='mx-auto border-2 border-black p-2'
+                            />
+                            <button
+                                className='bg-slate-300 hover:bg-slate-400 p-3 rounded-lg mx-2'
+                                onClick={handleSearch}
+                            >
+                                Search
+                            </button>
+                        </div>
 
-    return <>{!loading && (
-        <div>
-            <p className='mt-8 text-2xl text-center font-bold mb-4'>Requests from Clients</p>
-            <div className='flex flex-col text-xl gap-4 items-center'>
-                <div className='flex gap-2'>
-                    <input onChange={(e) => setSearch(e.target.value)}
-                        className='mx-auto border-2 border-black p-2'
-                    />
-                    <button className='bg-slate-300 hover:bg-slate-400 p-3 rounded-lg'
-                        onClick={handleSearch}
-                    >Search</button>
+                        <div className='table-container overflow-x-auto'>
+                            <table {...getTableProps()} className='border-collapse'>
+                                <thead>
+                                    {headerGroups.map((headerGroup) => (
+                                        <tr {...headerGroup.getHeaderGroupProps()}>
+                                            {headerGroup.headers.map((column) => (
+                                                <th
+                                                    {...column.getHeaderProps()}
+                                                    className='border'
+                                                    style={{ minWidth: column.minSize }}
+                                                >
+                                                    {column.render('Header')}
+                                                </th>
+                                            ))}
+                                        </tr>
+                                    ))}
+                                </thead>
+                                <tbody {...getTableBodyProps()}>
+                                    {rows.map((row) => {
+                                        prepareRow(row);
+                                        return (
+                                            <tr {...row.getRowProps()} className='border'>
+                                                {row.cells.map((cell) => (
+                                                    <td {...cell.getCellProps()} className='p-6'>
+                                                        {cell.render('Cell')}
+                                                    </td>
+                                                ))}
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-
-                {clientRequests.map((clientRequest) => (
-                    <div key={clientRequest.clientId} className='flex flex-col gap-2 p-4 w-[80vw] border-2 border-black'>
-                        <button onClick={() => handleButtonClick(clientRequest.name)}>{clientRequest.clientId} : {clientRequest.name}</button>
-                        {clientRequest.showDetails && (
-                            <div>
-                                <p>Client Name: {clientRequest.name}</p>
-                                <p>Client Email: {clientRequest.email}</p>
-                                <p>Client Number: {clientRequest.number}</p>
-                                <p>Client Address: {clientRequest.address}</p>
-                                <p>Date of Request: {clientRequest.date}</p>
-                                <p>Interested Aspects: {
-                                    clientRequest.aspects.join(', ')
-                                }</p>
-                                <p>Request Category: {clientRequest.option}</p>
-                                <p>How did Client get to know about us:{" "}
-                                    {clientRequest.sourceInfo !== 'other' && clientRequest.sourceInfo}
-                                    {clientRequest.sourceInfo === 'other' && clientRequest.specificInfo}
-                                </p>
-
-                                {clientRequest.option == 'measurement' &&
-                                    <div><p className='font-bold'>Measurement Request:</p>
-                                        <p>Cost: {clientRequest.measurementData.cost}</p>
-                                        <p>Date: {clientRequest.measurementData.date}</p>
-                                        <p>Time: {clientRequest.measurementData.time}</p>
-                                        <p>Supply/Fix: {clientRequest.measurementData.supplyFix}</p>
-                                        <p>Contact Person: {clientRequest.measurementData.contactPerson}</p>
-                                    </div>
-                                }
-                            </div>
-                        )}
-                    </div>))
-                }
-
-
-            </div>
-
-
-        </div>
-    )}</>
+            )}
+        </>
+    );
 }
