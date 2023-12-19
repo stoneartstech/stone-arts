32d54e942521e07dc633a21ed3bb8d3c200a994a
swapnilgarg7 <swapnilgarg810@gmail.com>
2023-12-19 16:40:01 +0300

    admin sales added

--- pages/saleshome.js
+++ pages/saleshome.js
@@ -2,10 +2,21 @@ import React from 'react'
 import SalesDashboard from '../components/SalesDashboard'
 
 export default function saleshome() {
+    const salesMails = {
+        'stoneartsgalleria@stonearts.com': "Galleria",
+        'stoneartsmirage@stonearts.com': "Mirage",
+        'stoneartskisumu@stonearts.com': "Kisumu",
+        'stoneartsmombasaroad@stonearts.com': "Mombasa Road",
+    }
     return (
         <div>
-            <p className='text-2xl text-center'>Sale Team Home Page</p>
-            <SalesDashboard />
+            <p className='text-4xl mb-6 text-center'>All Sales Teams</p>
+            <div className='flex flex-col'>
+                {Object.keys(salesMails).map((mail) => (
+                    <SalesDashboard showroomName={salesMails[mail]} />
+                ))}
+            </div>
+
         </div>
     )
 }
