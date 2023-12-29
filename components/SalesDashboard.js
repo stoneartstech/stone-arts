dde90b0d5db06fd31f177153ca734f3bc47df646
swapnilgarg7 <swapnilgarg810@gmail.com>
2023-12-29 17:45:45 +0300

    dynamic sales data

--- components/SalesDashboard.js
+++ components/SalesDashboard.js
@@ -2,18 +2,46 @@ import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import ClientHistory from './ClientHistory'
 import { AuthProvider, useAuth } from '../context/AuthContext'
+import { db } from '../firebase';
+import { collection, onSnapshot, doc, setDocs, setDoc, docRef } from 'firebase/firestore';
 import Link from 'next/link'
 
 export default function SalesDashboard({ showroomName }) {
 
-
+    const dbNames = {
+        "Galleria": "sales",
+        "Mirage": "mirage-sales",
+        "Kisumu": "kisumu-sales",
+        "Mombasa Road": "mombasa-sales",
+    }
+    const dbName = dbNames[showroomName]
+    const [visitedToday, setVisitedToday] = useState(0)
     const [salesData, setSalesData] = useState({
-        "Visited today": 0,
         "Called": 0,
         "Just visited": 0,
         "bought OTC": 0,
         "asked for quotes": 0,
     });
+    const date = new Date().toDateString()
+
+    useEffect(() => {
+        // Get data from firebase
+        const fetch = onSnapshot(collection(db, dbName), (snapshot) => {
+            var sales = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            sales = sales.filter((sale) => sale.id === date)
+            sales = sales[0]
+            console.log(sales)
+            if (sales) {
+                setSalesData(sales)
+                setVisitedToday(sales["Called"] + sales["Just visited"] + sales["bought OTC"] + sales["asked for quotes"])
+            }
+        })
+        return fetch
+    }
+        , [])
 
     const router = useRouter()
     function BOQHandler() {
@@ -35,18 +63,27 @@ export default function SalesDashboard({ showroomName }) {
         setSalesData(prevState => ({
             ...prevState,
             [key]: prevState[key] + 1,
-            "Visited today": prevState["Visited today"] + 1
+
+
         }));
+        setVisitedToday(visitedToday + 1)
+        updateSalesData()
     };
 
     const handleDecrement = (key) => {
         setSalesData(prevState => ({
             ...prevState,
             [key]: Math.max(prevState[key] - 1, 0), // Ensure the value doesn't go below 0
-            "Visited today": Math.max(prevState["Visited today"] - 1, 0)
         }));
+        setVisitedToday(visitedToday - 1)
 
     };
+
+    const updateSalesData = () => {
+        // Update data in firebase
+        const salesRef = doc(db, dbName, date)
+        setDoc(salesRef, salesData)
+    }
 
 
     return (
@@ -62,13 +99,13 @@ export default function SalesDashboard({ showroomName }) {
             <div className='flex flex-col sm:flex-row sm:p-24 items-center justify-center gap-8 sm:gap-24'>
                 <div className='flex flex-col text-xl gap-8'>
                     <div className='flex items-center gap-2'>
-                        <p className='text-center'>{salesData["Visited today"]} Visited today</p>
+                        <p className='text-center'>{visitedToday} Visited today</p>
                     </div>
                     {Object.keys(salesData).slice(1).map((key, index) => (
                         <div className='flex items-center gap-2' key={index}>
                             <p className='text-center'>{salesData[key]} {key}</p>
                             <button className='w-8 h-8 bg-green-400' onClick={() => handleIncrement(key)}> + </button>
-                            <button className='w-8 h-8 bg-red-400' onClick={() => handleDecrement(key)}> - </button>
+                            {/* <button className='w-8 h-8 bg-red-400' onClick={() => handleDecrement(key)}> - </button> */}
                         </div>
                     ))}
                 </div>
