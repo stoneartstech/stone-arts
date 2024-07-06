258775282dc4bb7f9c86257e73e4bc6e06e8fcf6
Milan Ram <milanram0780@gmail.com>
2024-07-06 10:18:58 +0530

    feature: img upload + close job card

--- pages/PMTHead/OngoingSites.js
+++ pages/PMTHead/OngoingSites.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { db } from "../../firebase";
+import { db, storage } from "../../firebase";
 import {
   collection,
   onSnapshot,
@@ -14,6 +14,7 @@ import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import { enqueueSnackbar, SnackbarProvider } from "notistack";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export default function OngoingSites() {
   const [isQsSelected, setIsQsSelected] = useState(false);
@@ -21,20 +22,18 @@ export default function OngoingSites() {
   const [actionsTab, setActionsTab] = useState(false);
   const [selectedClient, setSelectedClient] = useState("");
   const [tempAddress, setTempAddress] = useState("");
-  const [originalRequests, setOriginalRequests] = useState([]);
+  const [uploading, setUploading] = useState(false);
+  const [fileLink, setFileLink] = useState("");
+  const [uploadImage, setUploadImage] = useState("");
   const [requests, setRequests] = useState([]);
   const [qs, setQs] = useState(1);
   const router = useRouter();
 
   async function fetchCompletedQuotes() {
-    const qsRef = doc(db, "qs-count", "qs-count");
-    const maxQsCount = await getDoc(qsRef);
-    const maxQs = maxQsCount.data()["qs-count"]
-      ? maxQsCount.data()["qs-count"]
-      : 1;
-
+    setLoading(true);
     const promises = [];
-
+    // let { signedCopy, jobCardClosed, ...rest } = selectedClient;
+    // setDoc(doc(db, `PMT-sites-in-progress/${selectedClient?.clientId}`), rest);
     // for (let qs = 1; qs <= maxQs; qs++) {
     const requestsRef = collection(db, `PMT-sites-in-progress`);
     const snapshot = await getDocs(requestsRef);
@@ -50,6 +49,57 @@ export default function OngoingSites() {
     setRequests(combinedArray);
     return combinedArray;
   }
+  const handleUploadReportImages = async () => {
+    setUploading(true);
+    try {
+      const storageRef = ref(
+        storage,
+        `PMTImages/OngoingSites/SignedCopy/${selectedClient?.clientId}`
+      );
+      await uploadBytes(storageRef, uploadImage);
+
+      // Get the download URL for the uploaded file
+      const downloadURL = await getDownloadURL(storageRef);
+      setFileLink(downloadURL);
+      console.log("image Uploaded");
+      let tempObj = { ...selectedClient, signedCopy: downloadURL };
+      setDoc(
+        doc(db, `PMT-sites-in-progress/${selectedClient?.clientId}`),
+        tempObj
+      );
+      setSelectedClient(tempObj);
+
+      enqueueSnackbar(` File Uploaded Successfully`, {
+        variant: "success",
+      });
+      setUploadImage("");
+      setUploading(false);
+    } catch (error) {
+      enqueueSnackbar("Some error occured", {
+        variant: "error",
+      });
+      console.error(error);
+      setUploading(false);
+    }
+  };
+  const handleCloseJobCard = async () => {
+    try {
+      let tempObj = { ...selectedClient, jobCardClosed: true };
+      setDoc(
+        doc(db, `PMT-sites-in-progress/${selectedClient?.clientId}`),
+        tempObj
+      );
+      setSelectedClient(tempObj);
+      enqueueSnackbar(` JobCard Closed Successfully`, {
+        variant: "success",
+      });
+    } catch (error) {
+      enqueueSnackbar("Some error occured", {
+        variant: "error",
+      });
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     fetchCompletedQuotes()
@@ -94,8 +144,8 @@ export default function OngoingSites() {
               : "Sites In Progress"}
           </p>
           {selectedClient && (
-            <p className=" w-full text-center text-sm mt-0.5 ">
-              Start Date - ( {selectedClient?.date}, {selectedClient?.time} )
+            <p className=" w-full text-center text-sm mt-0.5 font-medium ">
+              Start Date: {selectedClient?.date}, {selectedClient?.time}
             </p>
           )}
         </div>
@@ -139,11 +189,16 @@ export default function OngoingSites() {
               </div>
             ) : (
               <div className=" flex flex-col items-center justify-center ">
-                <div className=" mt-4 grid grid-cols-2 flex-col gap-3 ">
+                <div className=" mt-4 grid grid-cols-2 flex-col gap-2.5 w-fit ">
+                  <div className=" flex  flex-col col-span-2 w-full">
+                    <p className=" bg-white capitalize text-center w-full text-xs md:text-base font-semibold py-1.5 md:py-2.5 px-4 border-black border ">
+                      Site Supervisor: {selectedClient?.supervisor}
+                    </p>
+                  </div>
                   <div className=" flex  flex-col col-span-2 w-full">
                     {selectedClient?.address ? (
                       <p className=" bg-white capitalize text-center w-full text-xs md:text-base font-semibold py-1.5 md:py-2.5 px-4 border-black border ">
-                        Address - {selectedClient?.address}
+                        Address: {selectedClient?.address}
                       </p>
                     ) : (
                       <div className=" flex items-center gap-2">
@@ -196,51 +251,88 @@ export default function OngoingSites() {
                   <Link
                     href={`/PMTHead/actions/CheckImages?qsName=${selectedClient?.qsName}&clientId=${selectedClient?.clientId}&clientName=${selectedClient?.name}&type=view`}
                   >
-                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  w-[300px]">
+                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  min-w-[300px] w-full">
                       Check Images
                     </button>
                   </Link>
                   <Link
                     href={`/PMTHead/actions/CheckProgress?qsName=${selectedClient?.qsName}&clientId=${selectedClient?.clientId}&clientName=${selectedClient?.name}`}
                   >
-                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  w-[300px]">
+                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  min-w-[300px] w-full">
                       Check Progress
                     </button>
                   </Link>
                   <Link href={selectedClient?.quotePdf} target="_blank">
-                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  w-[300px]">
+                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  min-w-[300px] w-full">
                       Check Quote
                     </button>
                   </Link>
                   <Link
                     href={`/PMTHead/actions/MaterialPlanUpload?qsName=${selectedClient?.qsName}&clientId=${selectedClient?.clientId}&clientName=${selectedClient?.name}&type=view`}
                   >
-                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  w-[300px]">
+                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  min-w-[300px] w-full">
                       Check Material Plan
                     </button>
                   </Link>
                   <Link
                     href={`/PMTHead/actions/SitePlanUpload?qsName=${selectedClient?.qsName}&clientId=${selectedClient?.clientId}&clientName=${selectedClient?.name}&type=view`}
                   >
-                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  w-[300px]">
+                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  min-w-[300px] w-full">
                       Check Site Plan
                     </button>
                   </Link>
                   <Link
                     href={`/PMTHead/actions/JobCards?qsName=${selectedClient?.qsName}&clientId=${selectedClient?.clientId}&clientName=${selectedClient?.name}&type=view`}
                   >
-                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  w-[300px]">
+                    <button className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  min-w-[300px] w-full">
                       Check Job Card
                     </button>
                   </Link>
-                  <div className=" flex  flex-col col-span-2 w-full mt-4">
-                    <p className=" text-center text-sm mb-0.5">
-                      (Site Supervisor)
-                    </p>
-                    <p className=" bg-white capitalize text-center w-full text-xs md:text-base font-semibold py-1.5 md:py-2.5 px-4 border-black border ">
-                      {selectedClient?.supervisor}
-                    </p>
-                  </div>
+                  {selectedClient?.signedCopy ? (
+                    <a
+                      href={selectedClient?.signedCopy}
+                      target="_blank"
+                      type="button"
+                      className=" bg-[#94e63d] hover:bg-[#83cb37] text-center text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  min-w-[300px] w-full"
+                    >
+                      View Signed Copy
+                    </a>
+                  ) : (
+                    <div className=" flex items-center">
+                      <input
+                        required
+                        className=" w-fit"
+                        id="file"
+                        name="file"
+                        type="file"
+                        onChange={(e) => {
+                          setUploadImage(e.target.files[0]);
+                        }}
+                      />
+                      <button
+                        disabled={uploading || uploadImage === ""}
+                        type="button"
+                        className=" disabled:bg-gray-400 font-semibold text-sm bg-green-500 p-2.5 rounded-lg w-[100px]"
+                        onClick={() => {
+                          handleUploadReportImages();
+                        }}
+                      >
+                        {uploading ? "Uploading" : "Upload   "}
+                      </button>
+                    </div>
+                  )}
+                  <button
+                    disabled={selectedClient?.jobCardClosed}
+                    onClick={() => {
+                      handleCloseJobCard();
+                    }}
+                    className=" bg-[#94e63d] disabled:bg-gray-300 disabled:text-gray-700 hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-2.5 px-4 border-black border  w-full"
+                  >
+                    {selectedClient?.jobCardClosed
+                      ? "Job Card Closed"
+                      : "Close Job Card"}
+                  </button>
+
                   <div className=" flex  flex-col col-span-2">
                     <button
                       onClick={() => {
@@ -263,7 +355,8 @@ export default function OngoingSites() {
                           console.error(error);
                         }
                       }}
-                      className=" bg-[#94e63d] hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-3 px-4 border-black border  w-full"
+                      disabled={!selectedClient?.jobCardClosed}
+                      className=" mt-2 bg-[#94e63d] disabled:bg-gray-300 disabled:text-gray-700 hover:bg-[#83cb37] text-xs md:text-sm font-semibold py-1.5 md:py-3 px-4 border-black border  w-full"
                     >
                       Complete Project
                     </button>
